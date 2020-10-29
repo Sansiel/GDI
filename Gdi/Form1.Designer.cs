f680bd166e1488592a7410660ac403307aecd26a
Sansiel <deviler.san@yandex.ru>
2020-10-29 13:59:07 +0400

    GDI_DONE

--- Gdi/Form1.Designer.cs
+++ Gdi/Form1.Designer.cs
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(27, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(669, 448);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(702, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Animation_Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(3, 466);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(832, 42);
+            this.panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(679, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Бегущий средь льда";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 520);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Gdi/Form1.cs
+++ Gdi/Form1.cs
@@ -1,20 +1,170 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Gdi
 {
     public partial class Form1 : Form
     {
+        int local_x = 300;
+        int local_y = 150;
+        int local_r = 0;
+        float local_s = 1;
+
+        Color start_color;
+        Color end_color;
+        //System.Threading.Timer timer;
+        System.Windows.Forms.Timer timer;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            start_color = label1.ForeColor;
+            end_color = Color.Red;
+            LoadData();
+        }
+        
+        private void LoadData()
+        {
+            Bitmap bm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            // sea
+            Graphics gs = Graphics.FromImage(bm);
+            Point pointSea1 = new Point(0, 0);
+            Point pointSea2 = new Point(pictureBox1.Width, 0);
+            Point pointSea3 = new Point(pictureBox1.Width, pictureBox1.Height);
+            Point pointSea4 = new Point(0, pictureBox1.Height);
+            Point[] seaPoints = { pointSea1, pointSea2, pointSea3, pointSea4 };
+
+            //Кисть Градиентная
+            Rectangle rectangle1 = new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height);
+            RectangleF convertedRectangle = rectangle1;
+            LinearGradientBrush myLinearGradientBrush = new LinearGradientBrush(convertedRectangle, Color.CadetBlue, Color.Blue, LinearGradientMode.Horizontal);
+            gs.FillPolygon(myLinearGradientBrush, seaPoints);
+
+            // boat
+            Graphics gb = Graphics.FromImage(bm);
+            Point point1 = new Point(0, 30);
+            Point point2 = new Point(23, 30);
+            Point point3 = new Point(23 , 0);
+            Point point4 = new Point(40 , 10);
+            Point point5 = new Point(27, 20);
+            Point point6 = new Point(27, 30);
+            Point point7 = new Point(50, 30);
+            Point point8 = new Point(40, 50);
+            Point point9 = new Point(10, 50);
+
+            gb.TranslateTransform(local_x, local_y);
+            gb.RotateTransform(local_r);
+            gb.ScaleTransform(local_s, local_s);
+
+            Point[] boatPoints = { point1, point2, point3, point4, point5, point6, point7, point8, point9 };            
+            gb.FillPolygon(Brushes.SaddleBrown, boatPoints);
+
+            //Кисть Штриховая
+            HatchBrush myHatchBrush = new HatchBrush(HatchStyle.Horizontal, Color.Black, Color.Brown);
+            Point[] boatP = { point1, point2, point7, point8, point9 };
+            gb.FillPolygon(myHatchBrush, boatP);
+
+            // flag
+            Point pointFlag1 = new Point(25, 0);
+            Point pointFlag2 = new Point(40, 10);
+            Point pointFlag3 = new Point(27, 20);
+            Point pointFlag4 = new Point(25, 20);
+            Point[] flagPoints = { pointFlag1, pointFlag2, pointFlag3, pointFlag4 };
+            // Ручка
+            Pen flagPen = new Pen(Brushes.Black, 2);
+            gb.FillPolygon(Brushes.White, flagPoints);
+            gb.DrawPolygon(flagPen, boatPoints);
+
+            // ice
+            Graphics gi = Graphics.FromImage(bm);
+            Point pointIce1 = new Point(50, 60);
+            Point pointIce2 = new Point(60, 40);
+            Point pointIce3 = new Point(65, 45);
+            Point pointIce4 = new Point(70, 30);
+            Point pointIce5 = new Point(75, 60);
+            Point[] icePoints = { pointIce1, pointIce2, pointIce3, pointIce4, pointIce5 };
+
+            //Кисть Сплошная
+            gi.FillPolygon(Brushes.LightBlue, icePoints);
+            gi.TranslateTransform(100, 50);
+            gi.FillPolygon(Brushes.LightBlue, icePoints);
+            gi.TranslateTransform(150, 150);
+            gi.FillPolygon(Brushes.LightBlue, icePoints);
+
+
+            pictureBox1.Image = bm;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.W) local_y -= 5;
+            if (e.KeyData == Keys.D) local_x += 5;
+            if (e.KeyData == Keys.A) local_x -= 5;
+            if (e.KeyData == Keys.S) local_y += 5;
+
+            if (e.KeyData == Keys.Q) local_r += 10;
+            if (e.KeyData == Keys.E) local_r -= 10;
+
+            if (e.KeyData == Keys.Z) local_s = local_s / 2;
+            if (e.KeyData == Keys.X) local_s = local_s * 2;
+
+            LoadData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //TimerCallback tm = new TimerCallback(Animo);
+            //timer = new System.Threading.Timer(tm, 0, 0, 300);
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 100;
+            timer.Tick += Animo;
+            timer.Start();
+        }
+
+        //private void Animo(object obj)
+        private void Animo(object sender, EventArgs e)
+        {
+            if (local_x >= 30)
+            {
+                local_x -= 1;
+                LoadData();
+
+                // running string
+                if (label1.ForeColor == start_color) label1.ForeColor = end_color;
+                else label1.ForeColor = start_color;
+                if (label1.Left > -label1.Width) label1.Left -= 10;
+                else label1.Left = this.Width;
+            }
+            else
+            {
+                //pictureBox1.Image = Image.FromFile("D:\\stop.jfif");
+                //timer.Dispose(); Console.WriteLine("stop_timer");
+                timer.Stop();
+            }
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.W) local_y -= 5;
+            if (e.KeyData == Keys.D) local_x += 5;
+            if (e.KeyData == Keys.A) local_x -= 5;
+            if (e.KeyData == Keys.S) local_y += 5;
+
+            if (e.KeyData == Keys.Q) local_r += 10;
+            if (e.KeyData == Keys.E) local_r -= 10;
+
+            if (e.KeyData == Keys.Z) local_s = local_s / 2;
+            if (e.KeyData == Keys.X) local_s = local_s * 2;
+
+            LoadData();
         }
     }
 }
